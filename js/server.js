50e86d01e4a09d48c8275b6d1113abc06614cc5d
Zach Miller <zmmille2@gmail.com>
2017-12-29 13:57:27 -0600

    young server

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.set('view engine', 'pug');
-//app.use(express.static('stylesheets'));
+app.use(express.static('stylesheets'));
 app.use(express.static(__dirname + '/public'));
 
 var port = 3000;
@@ -10,7 +10,11 @@ var port = 3000;
 console.log('listening on ' + port);
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
+
+app.get('*', function (req, res) {
+    res.render('404', { path: 'zmmille2.github.io' + req.path }); // can I get this programmatically?
+})
 
 app.listen(port);
