b520fad7194a9463d6cdcea7c816ee0de5e93db0
Zach Miller <zmmille2@gmail.com>
2018-01-15 15:35:40 -0800

    officially loading bootstrap in index.pug

--- js/server.js
+++ js/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public/img'));
+app.use(express.static('public/stylesheets'));
 
 var port = 3000;
 
