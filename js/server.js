cfdb6debe44a7eef0713a4c9488db26bb8f09485
Zach Miller <zmmille2@gmail.com>
2018-02-07 12:07:39 -0600

    added smarter path resolution, resume

--- js/server.js
+++ js/server.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+// var public = require('C:\\Users\\zamiller\\source\\repos\\firstproject\\public');
+var root = path.join(__dirname, '..');
+var public = path.join(root, 'public');
 
 var app = express();
 app.set('view engine', 'pug');
+//app.use(express.static('public'));
 app.use(express.static('public/img'));
 app.use(express.static('public/stylesheets'));
+//app.use(express.static('public'));
 
 var port = 3000;
 
 console.log('listening on ' + port);
+console.log(root);
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!' });
@@ -17,8 +25,17 @@ app.get('/blog', (req, res) => {
     res.render('blog', {});
 })
 
+app.get('/resume', (req, res) => {
+    var filePath = path.join(public, 'resume.pdf');
+
+    fs.readFile(filePath , function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+})
+
 app.get('*', function (req, res) {
-    res.render('404', { path: 'zmmille2.github.io' + req.path }); // can I get this programmatically?
+    res.render('404', { path: __dirname + req.path }); // can I get this programmatically?
 })
 
 app.listen(port);
